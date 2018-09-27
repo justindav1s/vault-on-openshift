68c2cee7cba3fb0792b4b1ed27bb84852ffa45f4
Justin Davis <justinndavis@gmail.com>
2018-09-27 16:27:50 +0100

    refactoring

--- vault/init_vault.sh
+++ vault/init_vault.sh
@@ -4,5 +4,8 @@ PROJECT=vault-controller
 
 oc project $PROJECT
 
-vault init -tls-skip-verify -key-shares=1 -key-threshold=1
+vault init -tls-skip-verify -key-shares=1 -key-threshold=1 > init.txt
 
+export KEYS=`cat init.txt | grep "Unseal Key" | awk '{print $4}'`
+
+export ROOT_TOKEN=`cat init.txt | grep "Root Token" | awk '{print $4}'`
