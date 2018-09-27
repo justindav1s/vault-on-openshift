28c1c155eb3d32f04e028f7609156e832a0b0b02
Justin Davis <justinndavis@gmail.com>
2018-09-27 11:57:19 +0100

    initial commit

--- spring-native-jd/src/main/java/com/raffa/SecretController.java
+++ spring-native-jd/src/main/java/com/raffa/SecretController.java
@@ -4,12 +4,18 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecretController {
 	
 	private Log log = LogFactory.getLog(SecretController.class);
+
+	@RequestMapping(value = "/health", method = RequestMethod.GET)
+	public String ping() {
+		return "OK";
+	}
 
 	@Value("${password}")
 	String password;
