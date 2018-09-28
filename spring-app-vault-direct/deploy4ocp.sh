305a44b9401c649a55f65acdde162ac72c0c618b
Justin Davis <justinndavis@gmail.com>
2018-09-28 14:26:03 +0100

    refactoring

--- spring-app-vault-direct/deploy4ocp.sh
+++ spring-app-vault-direct/deploy4ocp.sh
@@ -3,10 +3,11 @@
 PROJECT=spring-vault-demo
 
 oc new-app -f spring-vault-direct-template.yaml \
-    -p APP_NAME=test1 \
+    -p APP_NAME=test1-direct-vault \
+    -p VAULT_USERROLE=test1 \
     -p IMAGE_NAME=spring-vault-direct \
     -p VAULT_HOST=vault-vault-controller.apps.ocp.datr.eu \
     -p VAULT_PORT=443
 
-oc expose svc test1
+oc expose svc test1-direct-vault
 
