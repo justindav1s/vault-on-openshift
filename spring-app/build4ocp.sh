c7cc954b446f932d48f322bb18e3328a90230025
Justin Davis <justinndavis@gmail.com>
2018-09-28 15:43:40 +0100

    refactoring

--- spring-app/build4ocp.sh
+++ spring-app/build4ocp.sh
@@ -2,6 +2,10 @@
 
 PROJECT=spring-vault-demo
 
+oc project $PROJECT
+
+oc delete bc spring-app
+
 oc new-build \
     registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift~https://github.com/justindav1s/kube-vault-adapter \
     --context-dir=spring-app \
