50691f2105ba1f06bd2bdf8e8ae4c3fd83b25a55
Justin Davis <justinndavis@gmail.com>
2018-09-28 15:21:26 +0100

    refactoring

--- spring-vault-adapter/build4ocp.sh
+++ spring-vault-adapter/build4ocp.sh
@@ -2,8 +2,15 @@
 
 PROJECT=spring-vault-demo
 
+oc project $PROJECT
+
+oc delete bc spring-vault-adapter
+
 oc new-build \
     registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift~https://github.com/justindav1s/kube-vault-adapter \
     --context-dir=spring-vault-adapter \
     --name spring-vault-adapter
 
+sleep 5
+
+oc logs -f bc/spring-vault-adapter
