7d4070b5deb8ca6a90b51a67255d77528bb5abb9
Justin Davis <justinndavis@gmail.com>
2019-05-13 19:55:07 +0100

    updates for Vault version v1.1.2

--- spring-app-vault-direct/add_secret.sh
+++ spring-app-vault-direct/add_secret.sh
@@ -10,5 +10,5 @@ export VAULT_ADDR=https://`oc get route -n vault | grep -m1 vault | awk '{print 
 export ROOT_TOKEN=`cat ../vault_setup/root_token.txt| head -1`
 export VAULT_TOKEN=$ROOT_TOKEN
 
-vault write -tls-skip-verify secret/${APPNAME}/dev password=pwd_from_vault dbpassword3=pwd_from_vault
+vault write -tls-skip-verify secret/${APPNAME}/dev broker.password=pwd_for_broker db.password=pwd_for_db
 
