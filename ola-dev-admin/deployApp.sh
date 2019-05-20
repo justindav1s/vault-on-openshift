134061783cfab2e358299b2d1249bc134a895e8e
Justin Davis <justinndavis@gmail.com>
2019-05-20 15:03:48 +0100

    updates for Vault version v1.1.2

--- ola-dev-admin/deployApp.sh
+++ ola-dev-admin/deployApp.sh
@@ -22,7 +22,7 @@ oc new-app -f ../spring-app-vault-direct/spring-vault-direct-template.yaml \
     -p APP_NAME=${APPNAME} \
     -p APP_DOMAIN=${APPDOMAIN} \
     -p SPRING_PROFILES_ACTIVE=${ENV} \
-    -p VAULT_USERROLE=${APPDOMAIN}-${APPNAME} \
+    -p VAULT_USERROLE=${APPDOMAIN}-${ENV}-admin \
     -p IMAGE_NAME=spring-app-vault-direct \
     -p VAULT_HOST=vault-vault.apps.ocp.datr.eu \
     -p VAULT_PORT=443
