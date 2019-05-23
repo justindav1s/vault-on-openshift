183f373221fea1d49f1949408d770dec49d1cbe6
Justin Davis <justinndavis@gmail.com>
2019-05-23 19:16:06 +0100

    vault db stuff

--- spring-app-db-vault-direct/db/data.sh
+++ spring-app-db-vault-direct/db/data.sh
@@ -11,9 +11,8 @@ psql postgres demouser  << EOF
 CREATE TABLE person(PersonID int,lastName varchar(255),firstName varchar(255),address varchar(255),city varchar(255), PRIMARY KEY (PersonID));
 CREATE TABLE booking (personID int,bookingId int,flightnum varchar(255),tickets int,cabin  varchar(255), PRIMARY KEY (bookingId));
 CREATE TABLE flight(flightnum varchar(255),origin varchar(255),destination varchar(255),PRIMARY KEY (flightnum));
-
-INSERT INTO person (PersonID, lastName, firstName, address, city) VALUES (1, 'Davis', 'Justin', 'The Stooop', 'Twickenham');
 \d
+INSERT INTO person (PersonID, lastName, firstName, address, city) VALUES (1, 'Davis', 'Justin', 'The Stooop', 'Twickenham');
 
 SELECT * from person;
 
