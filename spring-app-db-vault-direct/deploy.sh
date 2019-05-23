68f785dbff09642fa69f8b7ab5adc598b5e5771c
Justin Davis <justinndavis@gmail.com>
2019-05-23 19:41:45 +0100

    vault db stuff

--- spring-app-db-vault-direct/deploy.sh
+++ spring-app-db-vault-direct/deploy.sh
@@ -9,7 +9,7 @@ oc project $PROJECT
 oc new-app -f spring-vault-direct-template.yaml \
     -p APP_NAME=${APPNAME} \
     -p VAULT_USERROLE=${APPDOMAIN}-${APPNAME} \
-    -p IMAGE_NAME=spring-app-vault-direct \
+    -p IMAGE_NAME=spring-app-db-vault-direct \
     -p VAULT_HOST=vault-vault.apps.ocp.datr.eu \
     -p VAULT_PORT=443
 
